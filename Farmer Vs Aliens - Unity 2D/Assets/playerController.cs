e7358738a0488bf40c4ff1f58026fb0fde834fc4
Eneri Junior <jr.icm.sc@gmail.com>
2020-03-28 16:45:05 -0300

    FVA

---
+++ Farmer Vs Aliens - Unity 2D/Assets/playerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    
+    private Rigidbody2D playerRb;
+    private Animator playerAnimator;
+
+    public float speed;
+    public float jumpForce;
+    public bool isLookLeft;
+    public Transform groudCheck;
+    private bool isGrounded;
+    private bool isAtack;
+    
+    
+    void Start() {
+
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        
+    }
+
+    
+    void Update() {
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if(h > 0 && isLookLeft == true){
+            Flip();
+        }
+        else if (h < 0 && isLookLeft == false){
+            Flip();
+        }
+
+        float speedY = playerRb.velocity.y; 
+
+        
+        if(Input.GetButtonDown("Jump") && isGrounded == true){
+
+            playerRb.AddForce(new Vector2(0,jumpForce));
+
+        }
+        
+        if(Input.GetButtonDown("Fire1") && isGrounded == true){
+            isAtack = true;
+            playerAnimator.SetTrigger("atack");
+
+        }
+
+        playerRb.velocity = new Vector2(h * speed, speedY);
+
+        playerAnimator.SetInteger("h",(int)h);
+        playerAnimator.SetBool("isGrounded", isGrounded);
+        playerAnimator.SetFloat("speedY", speedY);
+        playerAnimator.SetBool("isAtack",isAtack);
+        
+    }
+
+    void FixedUpdate() {
+        
+        isGrounded = Physics2D.OverlapCircle(groudCheck.position, 0.02f);
+    
+    }
+    void Flip(){
+        
+        isLookLeft = !isLookLeft;
+        float x = transform.localScale.x * -1; //inverte o sinal do scale x
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Jogo 2d sendo criado entre amigos.
+
+<img src="https://i.ibb.co/vXQYsfn/pp.jpg" alt="pp" border="0">
+
+<div><b>Título do Jogo?</b></div>
+    <div>Farmers vs Aliens.</div>
+
+<div><b>Plataforma ?</b></div>
+    <div>Mobile/PC</div>
+
+<div><b>Público-alvo ?</b></div>
+    <div>Jogadores Casuais e Fãs de RPG.</div>
+
+<div><b>Classificação Indicativa?</b></div>
+    <div>Em Análise!</div>
+    
+<div><b>Gênero do Jogo ?</b></div>
+    <div>Em Análise!</div>
+
+<div><b>Resumo do Conceito do Jogo?</b></div>
+    <div>Em Análise!</div>
+
+<div><b>Pontos de Destaques (Mecânicas que se destacam em seu jogo).</b></div>
+    <div>Em Análise!</div>
+
+
